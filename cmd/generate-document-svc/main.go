afe8af19576faca1589809195c853c15769649d8
Nils Diekmann <github@kinneko.de>
2023-07-12 19:04:10 +0200

    - naming schema version support

--- cmd/generate-document-svc/main.go
+++ cmd/generate-document-svc/main.go
@@ -4,12 +4,16 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/KinNeko-De/restaurant-document-svc/internal/app/operation"
+	root "github.com/kinneko-de/restaurant-generate-document-svc"
+
+	"github.com/KinNeko-De/restaurant-generate-document-svc/internal/app/operation"
 )
 
 func main() {
 	logfile := operation.UseLogFileInGenerated()
 	defer operation.CloseLogFile(logfile)
+
+	log.Println("Version " + root.Version)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
