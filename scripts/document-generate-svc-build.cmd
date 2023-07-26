ddadb8582fc0873b663fa182dd304840c586a320
Nils Diekmann <github@kinneko.de>
2023-07-27 00:19:46 +0200

    versiontxt

--- scripts/document-generate-svc-build.cmd
+++ scripts/document-generate-svc-build.cmd
@@ -2,8 +2,12 @@ pushd ..
 
 go mod download
 
+echo 0.0.1-local > build/version.txt
+
 set GOARCH=amd64
 set GOOS=linux
 go build -o scripts/sut/bin/app cmd/document-generate-svc/main.go
+
+echo set_by_ci > build/version.txt
 
 popd
