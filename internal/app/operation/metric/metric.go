a9ab4b718cfcf6f5a85c5b64014ec5101ec79847
Nils Diekmann <github@kinneko.de>
2023-09-30 12:33:46 +0200

    - extract config to environment variables - standardize env usage (according to documentation of the methods) - documented discrepancy between documentation and behavior

--- internal/app/operation/metric/metric.go
+++ internal/app/operation/metric/metric.go
@@ -2,6 +2,8 @@ package metric
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/operation/logger"
 	"go.opentelemetry.io/otel/attribute"
@@ -12,7 +14,12 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 )
 
+const ServiceNameEnv = "OTEL_SERVICE_NAME"
+const OtelMetricEndpointEnv = "OTEL_EXPORTER_OTLP_METRICS_ENDPOINT"
+
 var (
+	config            otelConfig
+	version           = "0.1.0"
 	ctx               = context.Background()
 	provider          *metric.MeterProvider
 	meter             api.Meter
@@ -21,8 +28,13 @@ var (
 	documentFailed    api.Int64Counter
 )
 
-func InitializeMetrics() (err error) {
-	otelReader, err := otlpmetricgrpc.New(ctx, otlpmetricgrpc.WithInsecure(), otlpmetricgrpc.WithEndpoint("otel-collector:4317"))
+func InitializeMetrics() error {
+	err := readConfig()
+	if err != nil {
+		logger.Logger.Fatal().Err(err).Msg("Failed to read metric reader configuration")
+	}
+
+	otelReader, err := otlpmetricgrpc.New(ctx, otlpmetricgrpc.WithInsecure(), otlpmetricgrpc.WithEndpoint(config.OtelMetricEndpoint))
 	if err != nil {
 		logger.Logger.Fatal().Err(err).Msg("Failed to initialize metric reader to otel collector")
 	}
@@ -37,7 +49,8 @@ func InitializeMetrics() (err error) {
 		metric.WithReader(metric.NewPeriodicReader(consoleReader)),
 	)
 
-	meter = provider.Meter("restaurant-document-generate-svc", api.WithInstrumentationVersion("0.1.0"))
+	// I decided to use the service name here as scope because this service is a microservice. one sccope per service approach.
+	meter = provider.Meter(config.OtelServiceName, api.WithInstrumentationVersion(version))
 
 	documentRequested, err = meter.Int64Counter("document-requested", api.WithUnit("document"), api.WithDescription("Number of documents requested"))
 	if err != nil {
@@ -53,7 +66,8 @@ func InitializeMetrics() (err error) {
 	if err != nil {
 		logger.Logger.Fatal().Err(err).Msg("Failed to initialize metric 'document-failed'")
 	}
-	return
+
+	return nil
 }
 
 func DocumentRequested(documentType string) {
@@ -70,4 +84,36 @@ func DocumentFailed(documentType string) {
 
 func ForceFlush() {
 	provider.ForceFlush(ctx)
+}
+
+func readConfig() error {
+	otelConfig, err := loadConfig()
+	if err != nil {
+		return err
+	}
+	config = otelConfig
+
+	return nil
+}
+
+type otelConfig struct {
+	OtelMetricEndpoint string // is used by the otel sdk to identify the endpoint to send metrics to. According to document it Will be set implicitly by the otel sdk. But it does not work. I set it explicitly.
+	OtelServiceName    string // is used by the otel sdk to identify the service name. I found no way to set it explicitly by the otel sdk. According to the specification setting an attribute with name "service.name" should work, but it does not.
+}
+
+func loadConfig() (otelConfig, error) {
+	endpoint, found := os.LookupEnv(OtelMetricEndpointEnv)
+	if !found {
+		return otelConfig{}, fmt.Errorf("otel metric endpoint is not configured. Expected environment variable %v", OtelMetricEndpointEnv)
+	}
+
+	serviceName, found := os.LookupEnv(ServiceNameEnv)
+	if !found {
+		return otelConfig{}, fmt.Errorf("otel service name is not configured. Expected environment variable %v", ServiceNameEnv)
+	}
+
+	return otelConfig{
+		OtelMetricEndpoint: endpoint,
+		OtelServiceName:    serviceName,
+	}, nil
 }
