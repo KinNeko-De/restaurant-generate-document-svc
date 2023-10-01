f8b6a5902058a166b868feae422e3ae40ef222c0
Nils Diekmann <github@kinneko.de>
2023-10-01 18:30:51 +0200

    - remove absoule path from cmd

--- tools/mockery/generate-mock.cmd
+++ tools/mockery/generate-mock.cmd
@@ -1,3 +1,5 @@
 pushd windows_x86_64
 
-mockery --dir C:\Git\github\kinneko-de\restaurant-document-generate-svc\internal\app\document --name DocumentGenerator --filename document_generator_mock.go --inpackage --with-expecter
+mockery --dir ../../../internal/app/document --name DocumentGenerator --filename document_generator_mock.go --inpackage --with-expecter
+
+popd
