0090dd845472b47bbffd8857b06707494beaddaa
Nils Diekmann <github@kinneko.de>
2023-11-03 00:35:08 +0100

    Graceful shutdown grpc server (#18)
    - add graceful shutdown for grpc service
     - add test for graceful shutdown

--- cmd/document-generate-svc/main.go
+++ cmd/document-generate-svc/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
@@ -12,36 +16,61 @@ import (
 	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/document"
 	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/operation/logger"
 	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/operation/metric"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-var (
-	logPanic recovery.RecoveryHandlerFunc
-)
-
 func main() {
-	logger.SetInfoLogLevel()
+	logger.SetLogLevel(zerolog.DebugLevel)
+	logger.Logger.Info().Msg("Starting application.")
 
 	provider, err := metric.InitializeMetrics()
 	if err != nil {
-		logger.Logger.Fatal().Err(err).Msg("failed to initialize metrics")
+		logger.Logger.Error().Err(err).Msg("failed to initialize metrics")
+		os.Exit(40)
 	}
-	defer provider.Shutdown(context.Background())
+	grpcServerStop := make(chan struct{})
+	go startGrpcServer(grpcServerStop, "3110")
 
-	StartGrpcServer()
+	<-grpcServerStop
+	provider.Shutdown(context.Background())
+	logger.Logger.Info().Msg("Application stopped.")
+	os.Exit(0)
 }
 
-func StartGrpcServer() *grpc.Server {
-	port := "3110"
+func startGrpcServer(grpcServerStop chan struct{}, port string) {
 	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		logger.Logger.Fatal().Err(err).Msgf("Failed to listen on port %v", port)
+		logger.Logger.Error().Err(err).Msgf("Failed to listen on port %v", port)
+		os.Exit(50)
 	}
 
 	// Handling of panic to prevent crash from example nil pointer exceptions
-	logPanic = func(p any) (err error) {
+	grpcServer := configureGrpcServer()
+
+	var gracefulStop = make(chan os.Signal, 1)
+	signal.Notify(gracefulStop, syscall.SIGTERM, syscall.SIGINT)
+	logger.Logger.Debug().Msg("starting grpc server")
+
+	go func() {
+		if err := grpcServer.Serve(listener); err != nil {
+			logger.Logger.Error().Err(err).Msg("failed to start grpc server")
+			os.Exit(51)
+		}
+	}()
+
+	stop := <-gracefulStop
+	grpcServer.GracefulStop()
+
+	logger.Logger.Debug().Msgf("http server stopped. Received signal %s", stop)
+	close(grpcServerStop)
+}
+
+func configureGrpcServer() *grpc.Server {
+	// Handling of panic to prevent crash from example nil pointer exceptions
+	logPanic := func(p any) (err error) {
 		log.Error().Any("method", p).Err(err).Msg("Recovered from panic.")
 		return status.Errorf(codes.Internal, "Internal server error occured.")
 	}
@@ -59,10 +88,6 @@ func StartGrpcServer() *grpc.Server {
 		),
 	)
 	RegisterAllGrpcServices(grpcServer)
-	if err := grpcServer.Serve(listener); err != nil {
-		logger.Logger.Fatal().Err(err).Msg("grpc server was aborted. Graceful shutdown should be implemented.")
-	}
-
 	return grpcServer
 }
 
