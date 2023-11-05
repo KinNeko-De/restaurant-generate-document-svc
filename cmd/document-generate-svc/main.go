9d64f15422ea92176aeac7f911fb4b1a760473a7
Nils Diekmann <github@kinneko.de>
2023-11-05 15:22:08 +0100

    - add grpc health checks

--- cmd/document-generate-svc/main.go
+++ cmd/document-generate-svc/main.go
@@ -2,24 +2,13 @@ package main
 
 import (
 	"context"
-	"net"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 
-	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
-
-	documentServiceApi "github.com/kinneko-de/api-contract/golang/kinnekode/restaurant/document/v1"
-	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/document"
+	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/operation/health"
 	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/operation/logger"
 	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/operation/metric"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func main() {
@@ -32,65 +21,14 @@ func main() {
 		os.Exit(40)
 	}
 	grpcServerStop := make(chan struct{})
-	go startGrpcServer(grpcServerStop, "3110")
+	grpcServerStarted := make(chan struct{})
+	go startGrpcServer(grpcServerStop, grpcServerStarted, "3110")
+
+	<-grpcServerStarted
+	health.Ready()
 
 	<-grpcServerStop
 	provider.Shutdown(context.Background())
 	logger.Logger.Info().Msg("Application stopped.")
 	os.Exit(0)
-}
-
-func startGrpcServer(grpcServerStop chan struct{}, port string) {
-	listener, err := net.Listen("tcp", ":"+port)
-	if err != nil {
-		logger.Logger.Error().Err(err).Msgf("Failed to listen on port %v", port)
-		os.Exit(50)
-	}
-
-	// Handling of panic to prevent crash from example nil pointer exceptions
-	grpcServer := configureGrpcServer()
-
-	var gracefulStop = make(chan os.Signal, 1)
-	signal.Notify(gracefulStop, syscall.SIGTERM, syscall.SIGINT)
-	logger.Logger.Debug().Msg("starting grpc server")
-
-	go func() {
-		if err := grpcServer.Serve(listener); err != nil {
-			logger.Logger.Error().Err(err).Msg("failed to start grpc server")
-			os.Exit(51)
-		}
-	}()
-
-	stop := <-gracefulStop
-	grpcServer.GracefulStop()
-
-	logger.Logger.Debug().Msgf("http server stopped. Received signal %s", stop)
-	close(grpcServerStop)
-}
-
-func configureGrpcServer() *grpc.Server {
-	// Handling of panic to prevent crash from example nil pointer exceptions
-	logPanic := func(p any) (err error) {
-		log.Error().Any("method", p).Err(err).Msg("Recovered from panic.")
-		return status.Errorf(codes.Internal, "Internal server error occured.")
-	}
-
-	opts := []recovery.Option{
-		recovery.WithRecoveryHandler(logPanic),
-	}
-
-	grpcServer := grpc.NewServer(
-		grpc.UnaryInterceptor(
-			recovery.UnaryServerInterceptor(opts...),
-		),
-		grpc.StreamInterceptor(
-			recovery.StreamServerInterceptor(opts...),
-		),
-	)
-	RegisterAllGrpcServices(grpcServer)
-	return grpcServer
-}
-
-func RegisterAllGrpcServices(grpcServer *grpc.Server) {
-	documentServiceApi.RegisterDocumentServiceServer(grpcServer, &document.DocumentServiceServer{})
 }
--- cmd/document-generate-svc/main_test.go
+++ cmd/document-generate-svc/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/exec"
 	"syscall"
@@ -10,6 +11,10 @@ import (
 	"github.com/kinneko-de/restaurant-document-generate-svc/internal/app/operation/metric"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	healthV1 "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func TestMain_MetricConfigIsMissing(t *testing.T) {
@@ -70,6 +75,8 @@ func TestMain_ProcessAlreadyListenToPort_AppCrash(t *testing.T) {
 	blockingcmd.Env = append(os.Environ(), "EXECUTE=1")
 	blockingErr := blockingcmd.Start()
 	require.Nil(t, blockingErr)
+	defer blockingcmd.Process.Kill()
+
 	time.Sleep(1 * time.Second) // give the service some time to start
 	cmd := exec.Command(os.Args[0], "-test.run=TestMain_ProcessAlreadyListenToPort_AppCrash")
 	cmd.Env = append(os.Environ(), "EXECUTE=1")
@@ -77,5 +84,77 @@ func TestMain_ProcessAlreadyListenToPort_AppCrash(t *testing.T) {
 	require.NotNil(t, err)
 	exitCode := err.(*exec.ExitError).ExitCode()
 	assert.Equal(t, 50, exitCode)
-	blockingcmd.Process.Kill()
+}
+
+func TestMain_HealthCheckIsServing_liveness(t *testing.T) {
+	serviceToCheck := "liveness"
+
+	if os.Getenv("EXECUTE") == "1" {
+		main()
+		return
+	}
+
+	t.Setenv(metric.OtelMetricEndpointEnv, "http://localhost")
+	t.Setenv(metric.ServiceNameEnv, "blub")
+	runningApp := exec.Command(os.Args[0], "-test.run=TestMain_HealthCheckIsServing")
+	runningApp.Env = append(os.Environ(), "EXECUTE=1")
+	blockingErr := runningApp.Start()
+	require.Nil(t, blockingErr)
+	defer runningApp.Process.Kill()
+
+	expectedStatus := healthV1.HealthCheckResponse_SERVING
+	healthResponse, err := waitForStatus(t, serviceToCheck, expectedStatus)
+
+	require.Nil(t, err)
+	require.NotNil(t, healthResponse)
+	assert.Equal(t, expectedStatus, healthResponse.Status)
+}
+
+func TestMain_HealthCheckIsServing_readiness(t *testing.T) {
+	serviceToCheck := "readiness"
+
+	if os.Getenv("EXECUTE") == "1" {
+		main()
+		return
+	}
+
+	t.Setenv(metric.OtelMetricEndpointEnv, "http://localhost")
+	t.Setenv(metric.ServiceNameEnv, "blub")
+	runningApp := exec.Command(os.Args[0], "-test.run=TestMain_HealthCheckIsServing")
+	runningApp.Env = append(os.Environ(), "EXECUTE=1")
+	blockingErr := runningApp.Start()
+	require.Nil(t, blockingErr)
+	defer runningApp.Process.Kill()
+
+	expectedStatus := healthV1.HealthCheckResponse_SERVING
+	healthResponse, err := waitForStatus(t, serviceToCheck, expectedStatus)
+
+	require.Nil(t, err)
+	require.NotNil(t, healthResponse)
+	assert.Equal(t, expectedStatus, healthResponse.Status)
+}
+
+func waitForStatus(t *testing.T, serviceToCheck string, expectedStatus healthV1.HealthCheckResponse_ServingStatus) (*healthV1.HealthCheckResponse, error) {
+	conn, dialErr := grpc.Dial("localhost:3110", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.Nil(t, dialErr)
+	defer conn.Close()
+
+	client := healthV1.NewHealthClient(conn)
+	count := 0
+	const iterations = 200
+	const interval = time.Millisecond * 10
+	var healthResponse *healthV1.HealthCheckResponse
+	var err error
+	for count < iterations {
+		healthResponse, err = client.Check(context.Background(), &healthV1.HealthCheckRequest{Service: serviceToCheck})
+		if healthResponse != nil && healthResponse.Status == expectedStatus {
+			t.Logf("health check succeeded after %v iterations", count)
+			break
+		} else {
+			t.Logf("health check failed after %v iterations", count)
+		}
+		time.Sleep(interval)
+		count++
+	}
+	return healthResponse, err
 }
