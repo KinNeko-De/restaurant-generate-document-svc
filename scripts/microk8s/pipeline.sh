3f482876fdf129df4323475f04533474c87e6cf4
Nils Diekmann <github@kinneko.de>
2023-11-11 12:04:14 +0100

    - example of error state

--- scripts/microk8s/pipeline.sh
+++ scripts/microk8s/pipeline.sh
@@ -9,4 +9,11 @@ sudo docker push localhost:32000/restaurant-document-generate-svc:latest
 
 kubectl apply --kustomize ./deployment/microk8s/overlays/dev
 
+kubectl apply --kustomize ./deployment/microk8s/overlays/image
+kubectl apply --kustomize ./deployment/microk8s/overlays/configerror
+kubectl apply --kustomize ./deployment/microk8s/overlays/crash
+kubectl apply --kustomize ./deployment/microk8s/overlays/notlive
+kubectl apply --kustomize ./deployment/microk8s/overlays/notready
+
+
 # kubectl logs -l app=document-generate-svc -tail 10 -n restaurant
